52d9be02bbd56465a856cc2526fa804f2821c575
Andreurp <andreurp96@gmail.com>
2015-09-26 09:00:51 +0200

    modificació mes gran

--- App.java
+++ App.java
@@ -7,20 +7,25 @@ public class App {
 	private static Scanner llagir;
 
 	public static void main(String[] args) {
-		int nombre1, nombre2, patit=0;
+		int nombre1, nombre2, patit, gran;
+		
 		System.out.println ("Escriu 5 numeros");
 		llagir = new Scanner(System.in);
 		nombre1 = llagir.nextInt();
-		
-		for (int i=1; i<5; i++){
+		patit = nombre1;
+		gran = nombre1;
+		for (int i=1; i<10; i++){
 			nombre2 = llagir.nextInt();
-				if (nombre1>nombre2){
+				if (patit>nombre2){
 					patit = nombre2;
-					nombre1 = patit;
+				}
+				if (gran<nombre2) {
+					gran = nombre2;
 				}
 		}
 		System.out.println ("El numero mes patit es: " + patit);
-		
+		System.out.println ("El numero mes gran es: " + gran);
 	}
 
 }
+
